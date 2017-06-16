179630cf53f44bd6cafacfac00ac1ca9226003f1
xaipo <xaipoj@hotmail.com>
2017-06-16 16:27:54 -0500

    change data conection
    change data conection with azure

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/managerboxBD',
+    database: 'mongodb://admin:123@52.168.13.191:27017/managerboxBD?connectTimeoutMS=30000&maxIdleTimeMS=600000&authMechanism=SCRAM-SHA-1',
     secret: 'yoursecret'
 }
